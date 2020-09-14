55ca350393ab9fb0cde9b7d246ea8aa53a80a529
qkrxo <qkrxo@192.168.0.122>
2020-09-14 15:50:42 +0900

    동일문서 수정 테스트-태준

--- src/kosta/mvc/view/view.java
+++ src/kosta/mvc/view/view.java
@@ -6,5 +6,11 @@ public class view {
 		
 		System.out.println("쉬는시간이 필요해");
 		System.out.println("동일한 java문서 수정");
+		
+		System.out.println("동일한 같은문서 저장 테스트1");
+		System.out.println("동일한 같은문서 저장 테스트2");
+		System.out.println("동일한 같은문서 저장 테스트3");
+		System.out.println("동일한 같은문서 저장 테스트4");
+		
 	}
 }
