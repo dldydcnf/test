58f802eb1211726fe17b3db2c229c49870f85749
qkrxo <qkrxo@192.168.0.122>
2020-09-14 15:45:43 +0900

    TJ 2차 수정

--- src/kosta/mvc/view/MainApp.java
+++ src/kosta/mvc/view/MainApp.java
@@ -4,7 +4,9 @@ public class MainApp {
 
 	public static void main(String[] args) {
 	System.out.println("test");
-
+	System.out.println("TJ수정");
+	System.out.println("TJ 2차 수정");
+	
+	new TestAA().aa();
 	}
-
 }
---
+++ src/kosta/mvc/view/TestAA.java
@@ -0,0 +1,9 @@
+package kosta.mvc.view;
+
+public class TestAA 
+{
+	public void aa()
+	{
+		System.out.println("에이에이");
+	}
+}
