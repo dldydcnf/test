96186eec76373cee3107245d7ed2e480d5b56c5b
qkrxo <qkrxo@192.168.0.122>
2020-09-14 15:49:16 +0900

    syso수정

--- src/kosta/mvc/view/view.java
+++ src/kosta/mvc/view/view.java
@@ -1,8 +1,10 @@
 package kosta.mvc.view;
 
 public class view {
-private void syso() {
-	// TODO Auto-generated method stub
-	System.out.println("쉬는시간이 필요해");
-}
+	private void viewview() 
+	{
+		
+		System.out.println("쉬는시간이 필요해");
+		System.out.println("동일한 java문서 수정");
+	}
 }
